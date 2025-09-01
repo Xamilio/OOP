cdd85ded5c31387bb7a3e0c60b205eefee9c21d0
Xamilio <Xamilio25@gmail.com>
2025-09-01 19:47:17 +0300

    Добавьте файлы проекта.

---
+++ Hw1/Hw1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+using namespace std;
+
+class Fraction
+{
+    int numerator;  
+    int denominator;
+
+public:
+    void Init(int a, int b)
+    {
+        numerator = a;
+        if (b == 0) {
+            cout << "Ошибка: знаменатель = 1" << endl;
+            denominator = 1;
+        }
+        else {
+            denominator = b;
+        }
+    }
+
+    void Init()
+    {
+        numerator = rand() % 20;
+        denominator = (rand() % 9) + 1;
+    }
+
+    void Print()
+    {
+        cout << "Дробь: " << numerator << "/" << denominator << endl;
+    }
+
+    void Sum(Fraction& b)
+    {
+        int num = numerator * b.denominator + b.numerator * denominator;
+        int den = denominator * b.denominator;
+        cout << "Сумма: " << num << "/" << den << endl;
+    }
+
+    void Sub(Fraction& b)
+    {
+        int num = numerator * b.denominator - b.numerator * denominator;
+        int den = denominator * b.denominator;
+        cout << "Разность: " << num << "/" << den << endl;
+    }
+
+    void Mul(Fraction& b)
+    {
+        int num = numerator * b.numerator;
+        int den = denominator * b.denominator;
+        cout << "Произведение: " << num << "/" << den << endl;
+    }
+
+    void Div(Fraction& b)
+    {
+        int num = numerator * b.denominator;
+        int den = denominator * b.numerator;
+        cout << "Частное: " << num << "/" << den << endl;
+    }
+};
+
+int main()
+{
+    Fraction A, B;
+
+    A.Init(2, 3);
+    A.Print();
+    B.Init(3, 4);
+    B.Print();
+    A.Sum(B);
+    A.Sub(B);
+    A.Mul(B);
+    A.Div(B);
+}
