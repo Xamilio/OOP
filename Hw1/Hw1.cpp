ccbc0ca1f1f2106f8fe7e1cc1b9a4e555ef5045a
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2025-09-14 22:00:04 +0300

    Update Hw1.cpp

--- Hw1/Hw1.cpp
+++ Hw1/Hw1.cpp
@@ -1,23 +1,32 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Group.h"
 using namespace std;
 
 int main()
 {
-    Group g("Group A", 2, 2);
-    Subject s1, s2;
-    s1.setName("Math");
-    s2.setName("Physics");
-    g.setSubject(0, s1);
-    g.setSubject(1, s2);
-    Student st1("Ivan", 2);
-    Student st2("Oleg", 2);
-    st1.setMark(0, 5);
-    st1.setMark(1, 4);
-    st2.setMark(0, 3);
-    st2.setMark(1, 5);
-    g.setStudent(0, st1);
-    g.setStudent(1, st2);
-    g.printMarks();
-    g.printAverages();
+    Group g;
+    g.setName("Group1");
+    g.setStudents(2);
+    g.setSubjects(2);
+
+    g.getSubject(0).setName("Math");
+    g.getSubject(1).setName("Physics");
+
+    g.getStudent(0).setName("Ivan");
+    g.getStudent(0).setMarks(g.getSubjectCount());
+    g.getStudent(0).setMark(0,5);
+    g.getStudent(0).setMark(1,4);
+
+    g.getStudent(1).setName("Petr");
+    g.getStudent(1).setMarks(g.getSubjectCount());
+    g.getStudent(1).setMark(0,3);
+    g.getStudent(1).setMark(1,5);
+
+    for(int i=0;i<g.getStudentCount();i++)
+    {
+        cout<<g.getStudent(i).getName()<<": ";
+        for(int j=0;j<g.getSubjectCount();j++)
+            cout<<g.getSubject(j).getName()<<"="<<g.getStudent(i).getMark(j)<<" ";
+        cout<<endl;
+    }
 }
