415077a9f3c6e38e8be2a279b90fdcffbdfc9858
Novak Sasha <Xamilio25@gmail.com>
2025-09-17 12:21:33 +0300

    String

--- Hw1/Hw1.cpp
+++ Hw1/Hw1.cpp
@@ -1,110 +1,45 @@
-﻿#include <iostream>
+﻿#include "MyString.h"
+#include <iostream>
 using namespace std;
 
-class Lift 
+int main()
 {
-    int minFloor;
-    int maxFloor;
-    int currentFloor;
-    bool isOn;
-public:
-    Lift(int minF = 1, int maxF = 9) : minFloor(minF), maxFloor(maxF), currentFloor(minF), isOn(false) 
+    MyString text1("Hello");
+    MyString text2("World");
+    text1.print();
+    text2.print();
+    MyString copy = text1;
+    copy.print();
+
+    if (text1.MyStrStr("el")) 
     {
-         
+        cout << "Substring found in Hello\n";
+    }
+    else 
+    {
+        cout << "Substring not found in Hello\n";
     }
 
-    void TurnOn()
-    {
-        isOn = true;
-        cout << "Лифт включен.\n";
-    }
-    void TurnOff()
-    {
-        isOn = false;
-        cout << "Лифт выключен.\n";
-    }
-    bool IsOn() const 
-    {
-        return isOn;
-    }
-    int GetCurrentFloor() const
-    {
-        return currentFloor;
-    }
-    void SetRange(int minF, int maxF)
-    {
-        if (minF < maxF) 
-        {
-            minFloor = minF;
-            maxFloor = maxF;
-            currentFloor = minF;
-        }
-    }
-    void Call(int floor) 
-    {
-        if (!isOn) 
-        {
-            cout << "Лифт не работает!\n";
-            return;
-        }
-        if (floor < minFloor || floor > maxFloor)
-        {
-            cout << "Ошибка: такого этажа нет!\n";
-            return;
-        }
-        cout << "Лифт движется с " << currentFloor << " этажа на " << floor << " этаж...\n";
-        currentFloor = floor;
-        cout << "Лифт прибыл на этаж " << currentFloor << ".\n";
-    }
-    void Print() 
-    {
-        cout << "---- Лифт ----\n";
-        cout << "Диапазон этажей: " << minFloor << " - " << maxFloor << endl;
-        cout << "Текущий этаж: " << currentFloor << endl;
-        cout << "Состояние: " << (isOn ? "Работает" : "Выключен") << endl;
-    }
-};
+    cout << "Index of 'o' in World: " << text2.MyChr('o') << endl;
+    cout << "Length of Hello: " << text1.MyStrLen() << endl;
+    text1.MyStrCat(text2);
+    cout << "After concatenation: ";
+    text1.print();
 
-int main() 
-{
-    setlocale(LC_ALL, "ru");
-    Lift lift(1, 5);
-
-    int choice;
-    do
+    text1.MyDelChr('l');
+    cout << "After deleting 'l':";
+    text1.print();
+    int result = text2.MyStrCmp(copy);
+    if (result == 0)
     {
-        cout << "\nМеню:\n";
-        cout << "1. Включить лифт\n";
-        cout << "2. Выключить лифт\n";
-        cout << "3. Вызвать лифт на этаж\n";
-        cout << "4. Показать состояние\n";
-        cout << "0. Выход\n";
-        cout << "Ваш выбор: ";
-        cin >> choice;
-
-        switch (choice) 
-        {
-        case 1:
-            lift.TurnOn();
-            break;
-        case 2:
-            lift.TurnOff();
-            break;
-        case 3: {
-            int floor;
-            cout << "Введите этаж: ";
-            cin >> floor;
-            lift.Call(floor);
-            break;
-        }
-        case 4:
-            lift.Print();
-            break;
-        case 0:
-            cout << "Программа завершена.\n";
-            break;
-        default:
-            cout << "Неверный выбор!\n";
-        }
-    } while (choice != 0);
+        cout << "World == Hello\n";
+    }
+    else if (result < 0)
+    {
+        cout << "World < Hello\n";
+    }
+    else
+    {
+        cout << "World > Hello\n";
+    }
 }
