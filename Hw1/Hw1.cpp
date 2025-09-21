c052e221be148d68df873047df46af9f6683cf57
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2025-09-21 16:46:07 +0300

    Update Hw1.cpp

--- Hw1/Hw1.cpp
+++ Hw1/Hw1.cpp
@@ -1,4 +1,4 @@
-﻿#include "MyString.h"
+#include "MyString.h"
 #include <iostream>
 using namespace std;
 
@@ -8,38 +8,28 @@ int main()
     MyString text2("World");
     text1.print();
     text2.print();
+
     MyString copy = text1;
     copy.print();
 
-    if (text1.MyStrStr("el")) 
-    {
+    if (text1.MyStrStr("el"))
         cout << "Substring found in Hello\n";
-    }
-    else 
-    {
+    else
         cout << "Substring not found in Hello\n";
-    }
 
     cout << "Index of 'o' in World: " << text2.MyChr('o') << endl;
     cout << "Length of Hello: " << text1.MyStrLen() << endl;
+
     text1.MyStrCat(text2);
     cout << "After concatenation: ";
     text1.print();
 
     text1.MyDelChr('l');
-    cout << "After deleting 'l':";
+    cout << "After deleting 'l': ";
     text1.print();
+
     int result = text2.MyStrCmp(copy);
-    if (result == 0)
-    {
-        cout << "World == Hello\n";
-    }
-    else if (result < 0)
-    {
-        cout << "World < Hello\n";
-    }
-    else
-    {
-        cout << "World > Hello\n";
-    }
+    if (result == 0) cout << "World == Hello\n";
+    else if (result < 0) cout << "World < Hello\n";
+    else cout << "World > Hello\n";
 }
