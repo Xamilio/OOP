4837b3f10b04c86afa5675db4df3cc966f5e36f7
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2025-09-24 14:06:40 +0300

    Update MyString.h

--- Hw1/MyString.h
+++ Hw1/MyString.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+
 class MyString
 {
     char* str;
@@ -10,6 +12,7 @@ public:
     ~MyString();
     MyString(const MyString& other);
     MyString(MyString&& other);
+
     void print();
     void MyStrcpy(MyString& obj);
     bool MyStrStr(const char* str);
@@ -18,4 +21,13 @@ public:
     void MyStrCat(MyString& b);
     void MyDelChr(char c);
     int MyStrCmp(MyString& b);
+
+    MyString& operator+=(const char* s);
+    MyString& operator-=(const char* s);
+    bool operator==(const MyString& other) const;
+    bool operator!=(const MyString& other) const;
+    bool operator>(const MyString& other) const;
+    bool operator<(const MyString& other) const;
+    MyString operator++(int);
+    MyString operator--(int);
 };
