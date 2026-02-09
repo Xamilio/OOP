a7cfe2a1c191d4f2bc69f2dc1529eaf8432147e7
Novak Sasha <Xamilio25@gmail.com>
2026-02-09 11:42:00 +0200

    prac1

--- Hw1/Prac6.cpp
+++ Hw1/Prac6.cpp
@@ -8,32 +8,37 @@ using namespace std;
 
 int main()
 {
-	Car car;
-	Track track;
-	Ship ship;
-	Airplane airplane;
+	int n;
+	cout << "Enter the type of transport (1 - car, 2 - track, 3 - ship, 4 - airplane): ";
+	cin >> n;
+	switch n
+	{
+		case 1:
+		{
+			Transport car;
+			car.Input();
+			car.Output();
+		}
+		case 2:
+		{
+			Transport Track;
+			track.Input();
+			track.Output();
+		}
+		case 3:
+		{
+			Transport Ship;
+			ship.Input();
+			ship.Output();
+		}
+		case 4:
+		{
+			Transport Airplane;
+			airplane.Input();
+			airplane.Output();
+		}
+		default:
+			cout << "Error";
+	}
 
-	cout << "Input car data:" << endl;
-	car.Input();
-	cout << endl;
-	cout << "Input track data:" << endl;
-	track.Input();
-	cout << endl;
-	cout << "Input ship data:" << endl;
-	ship.Input();
-	cout << endl;
-	cout << "Input airplane data:" << endl;
-	airplane.Input();
-	cout << endl;
-	cout << "Car data:" << endl;
-	car.Print();
-	cout << endl;
-	cout << "Track data:" << endl;
-	track.Print();
-	cout << endl;
-	cout << "Ship data:" << endl;
-	ship.Print();
-	cout << endl;
-	cout << "Airplane data:" << endl;
-	airplane.Print();
 }
