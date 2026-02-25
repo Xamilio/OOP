0e618e6ccf79a2564a8b231f75f1533fd86c8163
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2026-02-26 00:07:40 +0200

    Refactor Car class into Buffer class with RAII

--- Hw1/Prac6.cpp
+++ Hw1/Prac6.cpp
@@ -1,113 +1,71 @@
 #include <iostream>
-#include <thread>
-#include <semaphore>
-#include <mutex>
-#include <fstream>
-#include <chrono>
+#include <cstring>
 
-using namespace std;
-
-class Car {
+class Buffer {
 private:
-    string name;
-    string number;
+    char* data_;
+    size_t size_;
 
 public:
-    Car(string n, string num) : name(n), number(num) {}
+    explicit Buffer(size_t size)
+        : data_(new char[size]), size_(size) {}
 
-    void park(counting_semaphore<3>& parking) {
-        cout << "Машина " << name << " (" << number << ") подъехала к парковке\n";
-        parking.acquire();
-        cout << "Машина " << name << " припарковалась\n";
-        this_thread::sleep_for(chrono::seconds(1));
-        cout << "Машина " << name << " уехала\n";
-        parking.release();
+    ~Buffer() {
+        delete[] data_;
+    }
+
+    Buffer(const Buffer&) = delete;
+    Buffer& operator=(const Buffer&) = delete;
+
+    Buffer(Buffer&& other) noexcept
+        : data_(other.data_), size_(other.size_) {
+        other.data_ = nullptr;
+        other.size_ = 0;
+    }
+
+    Buffer& operator=(Buffer&& other) noexcept {
+        if (this != &other) {
+            delete[] data_;
+
+            data_ = other.data_;
+            size_ = other.size_;
+
+            other.data_ = nullptr;
+            other.size_ = 0;
+        }
+        return *this;
+    }
+
+    char* data() {
+        return data_;
+    }
+
+    const char* data() const {
+        return data_;
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+    char* release() {
+        char* temp = data_;
+        data_ = nullptr;
+        size_ = 0;
+        return temp;
     }
 };
 
-mutex fileMutex;
-
-void writeToFile(int id, counting_semaphore<2>& fileSemaphore) {
-    cout << "Поток " << id << " начал работу\n";
-    fileSemaphore.acquire();
-
-    {
-        lock_guard<mutex> lock(fileMutex);
-        ofstream file("log.txt", ios::app);
-        file << "Поток " << id << " записал данные\n";
-        file.close();
-    }
-
-    cout << "Поток " << id << " закончил запись\n";
-    fileSemaphore.release();
-}
-
-int data[20] = {0};
-mutex arrayMutex;
-
-void fillArray(int id, counting_semaphore<2>& arraySemaphore) {
-    while (true) {
-        arraySemaphore.acquire();
-
-        int index = -1;
-
-        {
-            lock_guard<mutex> lock(arrayMutex);
-            for (int i = 0; i < 20; i++) {
-                if (data[i] == 0) {
-                    data[i] = id;
-                    index = i;
-                    break;
-                }
-            }
-        }
-
-        arraySemaphore.release();
-
-        if (index == -1)
-            break;
-
-        this_thread::sleep_for(chrono::milliseconds(100));
-    }
-}
-
 int main() {
+    Buffer buf(50);
 
-    counting_semaphore<3> parking(3);
-    thread cars[6];
+    std::strcpy(buf.data(), "RAII Buffer example");
+    std::cout << "Data: " << buf.data() << std::endl;
+    std::cout << "Size: " << buf.size() << std::endl;
 
-    for (int i = 0; i < 6; i++) {
-        Car car("Car" + to_string(i + 1), "AA00" + to_string(i + 1));
-        cars[i] = thread(&Car::park, car, ref(parking));
-    }
+    Buffer buf2 = std::move(buf);
 
-    for (int i = 0; i < 6; i++)
-        cars[i].join();
-
-
-    counting_semaphore<2> fileSemaphore(2);
-    thread writers[6];
-
-    for (int i = 0; i < 6; i++)
-        writers[i] = thread(writeToFile, i + 1, ref(fileSemaphore));
-
-    for (int i = 0; i < 6; i++)
-        writers[i].join();
-
-    counting_semaphore<2> arraySemaphore(2);
-    thread workers[5];
-
-    for (int i = 0; i < 5; i++)
-        workers[i] = thread(fillArray, i + 1, ref(arraySemaphore));
-
-    for (int i = 0; i < 5; i++)
-        workers[i].join();
-
-    cout << "\nМассив:\n";
-    for (int i = 0; i < 20; i++)
-        cout << data[i] << " ";
-
-    cout << endl;
+    std::cout << "After move: " << buf2.data() << std::endl;
 
     return 0;
 }
