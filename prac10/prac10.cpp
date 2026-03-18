a3f1756accfa03da46cd7a2223120aa170851cb3
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2026-03-18 13:18:33 +0200

    Update prac10.cpp

--- prac10/prac10.cpp
+++ prac10/prac10.cpp
@@ -1,84 +1,234 @@
 #include <iostream>
-#include <cstddef>
+#include <string>
+#include <vector>
+using namespace std;
 
-class Buffer
+class Context
 {
-private:
-    char* ptr;
-    std::size_t size_;
-
 public:
-    Buffer(std::size_t size)
+    int input;
+    string output;
+
+    Context(int input)
     {
-        size_ = size;
-        ptr = new char[size_];
+        this->input = input;
     }
 
-    ~Buffer()
+    Context(string output)
     {
-        delete[] ptr;
-    }
-
-    Buffer(const Buffer&) = delete;
-    Buffer& operator=(const Buffer&) = delete;
-
-    Buffer(Buffer&& other)
-    {
-        ptr = other.ptr;
-        size_ = other.size_;
-
-        other.ptr = nullptr;
-        other.size_ = 0;
-    }
-
-    Buffer& operator=(Buffer&& other)
-    {
-        if (this != &other)
-        {
-            delete[] ptr;
-
-            ptr = other.ptr;
-            size_ = other.size_;
-
-            other.ptr = nullptr;
-            other.size_ = 0;
-        }
-        return *this;
-    }
-
-    char* data()
-    {
-        return ptr;
-    }
-
-    std::size_t size() const
-    {
-        return size_;
-    }
-
-    char* release()
-    {
-        char* temp = ptr;
-        ptr = nullptr;
-        size_ = 0;
-        return temp;
+        this->input = 0;
+        this->output = output;
     }
 };
 
-int main()
+class Expression
 {
-    Buffer buf(5);
+public:
+    virtual void interpret(Context& context) = 0;
+};
 
-    buf.data()[0] = 'H';
-    buf.data()[1] = 'e';
-    buf.data()[2] = 'l';
-    buf.data()[3] = 'l';
-    buf.data()[4] = 'o';
+class RomanExpression : public Expression
+{
+protected:
+    virtual string one() = 0;
+    virtual string four() = 0;
+    virtual string five() = 0;
+    virtual string nine() = 0;
+    virtual int multiplier() = 0;
 
-    for (std::size_t i = 0; i < buf.size(); i++)
+public:
+    void interpret(Context& context) override
     {
-        std::cout << buf.data()[i];
+        int value = context.input / multiplier();
+        context.input %= multiplier();
+
+        if (value == 9)
+        {
+            context.output += nine();
+        }
+        else if (value >= 5)
+        {
+            context.output += five();
+
+            for (int i = 0; i < value - 5; i++)
+            {
+                context.output += one();
+            }
+        }
+        else if (value == 4)
+        {
+            context.output += four();
+        }
+        else
+        {
+            for (int i = 0; i < value; i++)
+            {
+                context.output += one();
+            }
+        }
+    }
+};
+
+class Thousand : public RomanExpression
+{
+protected:
+    string one() override
+    {
+        return "M";
     }
 
-    std::cout << std::endl;
+    string four() override
+    {
+        return "";
+    }
+
+    string five() override
+    {
+        return "";
+    }
+
+    string nine() override
+    {
+        return "";
+    }
+
+    int multiplier() override
+    {
+        return 1000;
+    }
+};
+
+class Hundred : public RomanExpression
+{
+protected:
+    string one() override
+    {
+        return "C";
+    }
+
+    string four() override
+    {
+        return "CD";
+    }
+
+    string five() override
+    {
+        return "D";
+    }
+
+    string nine() override
+    {
+        return "CM";
+    }
+
+    int multiplier() override
+    {
+        return 100;
+    }
+};
+
+class Ten : public RomanExpression
+{
+protected:
+    string one() override
+    {
+        return "X";
+    }
+
+    string four() override
+    {
+        return "XL";
+    }
+
+    string five() override
+    {
+        return "L";
+    }
+
+    string nine() override
+    {
+        return "XC";
+    }
+
+    int multiplier() override
+    {
+        return 10;
+    }
+};
+
+class One : public RomanExpression
+{
+protected:
+    string one() override
+    {
+        return "I";
+    }
+
+    string four() override
+    {
+        return "IV";
+    }
+
+    string five() override
+    {
+        return "V";
+    }
+
+    string nine() override
+    {
+        return "IX";
+    }
+
+    int multiplier() override
+    {
+        return 1;
+    }
+};
+
+int romanToInt(string s)
+{
+    vector<pair<string, int>> roman =
+    {
+        {"M",1000}, {"CM",900}, {"D",500}, {"CD",400},
+        {"C",100}, {"XC",90}, {"L",50}, {"XL",40},
+        {"X",10}, {"IX",9}, {"V",5}, {"IV",4}, {"I",1}
+    };
+
+    int result = 0;
+
+    for (auto& p : roman)
+    {
+        while (s.find(p.first) == 0)
+        {
+            result += p.second;
+            s.erase(0, p.first.length());
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    int number = 1994;
+    Context context(number);
+
+    vector<Expression*> tree;
+
+    tree.push_back(new Thousand());
+    tree.push_back(new Hundred());
+    tree.push_back(new Ten());
+    tree.push_back(new One());
+
+    for (auto exp : tree)
+    {
+        exp->interpret(context);
+    }
+
+    cout << "Arabic: " << number << " -> Roman: " << context.output << endl;
+
+    string roman = "MCMXCIV";
+
+    cout << "Roman: " << roman
+        << " -> Arabic: " << romanToInt(roman) << endl;
 }
