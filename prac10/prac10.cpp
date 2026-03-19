aa3cbdafa9b48db48919440b5005b3ecb6d79ea0
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2026-03-19 13:46:42 +0200

    Update prac10.cpp

--- prac10/prac10.cpp
+++ prac10/prac10.cpp
@@ -1,234 +1,95 @@
 #include <iostream>
-#include <string>
-#include <vector>
+
 using namespace std;
 
-class Context
-{
+class Observer {
 public:
-    int input;
-    string output;
-
-    Context(int input)
-    {
-        this->input = input;
-    }
-
-    Context(string output)
-    {
-        this->input = 0;
-        this->output = output;
-    }
+    virtual void update(float temperature) = 0;
+    virtual ~Observer() {}
 };
 
-class Expression
-{
-public:
-    virtual void interpret(Context& context) = 0;
-};
-
-class RomanExpression : public Expression
-{
+class Subject {
 protected:
-    virtual string one() = 0;
-    virtual string four() = 0;
-    virtual string five() = 0;
-    virtual string nine() = 0;
-    virtual int multiplier() = 0;
+    Observer* observers[10];
+    int count = 0;
 
 public:
-    void interpret(Context& context) override
-    {
-        int value = context.input / multiplier();
-        context.input %= multiplier();
+    void attach(Observer* obs) {
+        if (count < 10) {
+            observers[count++] = obs;
+        }
+    }
 
-        if (value == 9)
-        {
-            context.output += nine();
-        }
-        else if (value >= 5)
-        {
-            context.output += five();
-
-            for (int i = 0; i < value - 5; i++)
-            {
-                context.output += one();
-            }
-        }
-        else if (value == 4)
-        {
-            context.output += four();
-        }
-        else
-        {
-            for (int i = 0; i < value; i++)
-            {
-                context.output += one();
+    void detach(Observer* obs) {
+        for (int i = 0; i < count; i++) {
+            if (observers[i] == obs) {
+                for (int j = i; j < count - 1; j++) {
+                    observers[j] = observers[j + 1];
+                }
+                count--;
+                break;
             }
         }
     }
-};
 
-class Thousand : public RomanExpression
-{
-protected:
-    string one() override
-    {
-        return "M";
-    }
-
-    string four() override
-    {
-        return "";
-    }
-
-    string five() override
-    {
-        return "";
-    }
-
-    string nine() override
-    {
-        return "";
-    }
-
-    int multiplier() override
-    {
-        return 1000;
-    }
-};
-
-class Hundred : public RomanExpression
-{
-protected:
-    string one() override
-    {
-        return "C";
-    }
-
-    string four() override
-    {
-        return "CD";
-    }
-
-    string five() override
-    {
-        return "D";
-    }
-
-    string nine() override
-    {
-        return "CM";
-    }
-
-    int multiplier() override
-    {
-        return 100;
-    }
-};
-
-class Ten : public RomanExpression
-{
-protected:
-    string one() override
-    {
-        return "X";
-    }
-
-    string four() override
-    {
-        return "XL";
-    }
-
-    string five() override
-    {
-        return "L";
-    }
-
-    string nine() override
-    {
-        return "XC";
-    }
-
-    int multiplier() override
-    {
-        return 10;
-    }
-};
-
-class One : public RomanExpression
-{
-protected:
-    string one() override
-    {
-        return "I";
-    }
-
-    string four() override
-    {
-        return "IV";
-    }
-
-    string five() override
-    {
-        return "V";
-    }
-
-    string nine() override
-    {
-        return "IX";
-    }
-
-    int multiplier() override
-    {
-        return 1;
-    }
-};
-
-int romanToInt(string s)
-{
-    vector<pair<string, int>> roman =
-    {
-        {"M",1000}, {"CM",900}, {"D",500}, {"CD",400},
-        {"C",100}, {"XC",90}, {"L",50}, {"XL",40},
-        {"X",10}, {"IX",9}, {"V",5}, {"IV",4}, {"I",1}
-    };
-
-    int result = 0;
-
-    for (auto& p : roman)
-    {
-        while (s.find(p.first) == 0)
-        {
-            result += p.second;
-            s.erase(0, p.first.length());
+    void notify(float temperature) {
+        for (int i = 0; i < count; i++) {
+            observers[i]->update(temperature);
         }
     }
+};
 
-    return result;
-}
+class TemperatureSensor : public Subject {
+private:
+    float temperature;
 
-int main()
-{
-    int number = 1994;
-    Context context(number);
-
-    vector<Expression*> tree;
-
-    tree.push_back(new Thousand());
-    tree.push_back(new Hundred());
-    tree.push_back(new Ten());
-    tree.push_back(new One());
-
-    for (auto exp : tree)
-    {
-        exp->interpret(context);
+public:
+    void setTemperature(float temp) {
+        temperature = temp;
+        notify(temperature);
     }
 
-    cout << "Arabic: " << number << " -> Roman: " << context.output << endl;
+    float getTemperature() {
+        return temperature;
+    }
+};
 
-    string roman = "MCMXCIV";
+class ConsoleDisplay : public Observer {
+public:
+    void update(float temperature) override {
+        cout << "Температура: " << temperature << endl;
+    }
+};
 
-    cout << "Roman: " << roman
-        << " -> Arabic: " << romanToInt(roman) << endl;
-}
+class Logger : public Observer {
+public:
+    void update(float temperature) override {
+        cout << "[LOG] Новая температура: " << temperature << endl;
+    }
+};
+
+class Alarm : public Observer {
+public:
+    void update(float temperature) override {
+        if (temperature >= 30) {
+            cout << "!!! ВНИМАНИЕ: высокая температура !!!" << endl;
+        }
+    }
+};
+
+int main() {
+    TemperatureSensor sensor;
+
+    ConsoleDisplay display;
+    Logger logger;
+    Alarm alarm;
+
+    sensor.attach(&display);
+    sensor.attach(&logger);
+    sensor.attach(&alarm);
+
+    sensor.setTemperature(25);
+    sensor.setTemperature(30);
+    sensor.setTemperature(35);
+
+    return 0;
