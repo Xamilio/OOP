eb1ca1c598416a0a8d42496f13ec8bb7b3346cb9
Oleksandr <119781901+Xamilio@users.noreply.github.com>
2026-03-24 23:02:44 +0200

    Update Hw1.cpp

--- Hw1/Hw1.cpp
+++ Hw1/Hw1.cpp
@@ -1,97 +1,61 @@
 #include <iostream>
-#include <string>
-
+#include <memory>
 using namespace std;
 
-class Component {
+class DeliveryStrategy {
 public:
-    virtual int getPrice() = 0;
-    virtual void add(Component* c) {}
-    virtual void print(string indent = "") = 0;
-    virtual ~Component() {}
+    virtual double calculateCost(double distance, double weight) = 0;
+    virtual ~DeliveryStrategy() {}
 };
 
-class Item : public Component {
-    string name;
-    int price;
+class CourierDelivery : public DeliveryStrategy {
 public:
-    Item(string n, int p) {
-        name = n;
-        price = p;
-    }
-
-    int getPrice() override {
-        return price;
-    }
-
-    void print(string indent = "") override {
-        cout << indent << name << " : " << price << endl;
+    double calculateCost(double distance, double weight) override {
+        return 50 + 10 * distance;
     }
 };
 
-class Composite : public Component {
-    string name;
-    Component* children[100];
-    int count;
+class PickupDelivery : public DeliveryStrategy {
 public:
-    Composite(string n) {
-        name = n;
-        count = 0;
+    double calculateCost(double distance, double weight) override {
+        return 0;
+    }
+};
+
+class PostDelivery : public DeliveryStrategy {
+public:
+    double calculateCost(double distance, double weight) override {
+        return 25 + 5 * weight;
+    }
+};
+
+class Order {
+private:
+    unique_ptr<DeliveryStrategy> delivery;
+    double distance;
+    double weight;
+public:
+    Order(double dist, double w, DeliveryStrategy* del) 
+        : distance(dist), weight(w), delivery(del) {}
+
+    void setDelivery(DeliveryStrategy* del) {
+        delivery.reset(del);
     }
 
-    void add(Component* c) override {
-        children[count++] = c;
-    }
-
-    int getPrice() override {
-        int sum = 0;
-        for (int i = 0; i < count; i++) {
-            sum += children[i]->getPrice();
-        }
-        return sum;
-    }
-
-    void print(string indent = "") override {
-        cout << indent << name << endl;
-        for (int i = 0; i < count; i++) {
-            children[i]->print(indent + "  ");
-        }
+    double getDeliveryCost() {
+        return delivery->calculateCost(distance, weight);
     }
 };
 
 int main() {
-    Composite* office = new Composite("Офис");
+    Order order1(10, 2, new CourierDelivery());
+    cout << order1.getDeliveryCost() << endl;
 
-    Composite* reception = new Composite("Приемная");
-    reception->add(new Item("Журнальный столик", 200));
-    reception->add(new Item("Мягкий диван", 500));
-    reception->add(new Item("Стол секретаря", 300));
-    reception->add(new Item("Кулер", 150));
+    order1.setDelivery(new PickupDelivery());
+    cout << order1.getDeliveryCost() << endl;
 
-    Composite* room1 = new Composite("Аудитория 1");
-    room1->add(new Item("10 столов", 1000));
-    room1->add(new Item("Доска", 200));
-    room1->add(new Item("Стол учителя", 300));
-
-    Composite* computerRoom = new Composite("Компьютерная аудитория");
-    Composite* tables = new Composite("10 компьютерных столов");
-
-    for (int i = 0; i < 10; i++) {
-        Composite* table = new Composite("Стол с компьютером");
-        table->add(new Item("Компьютер", 800));
-        tables->add(table);
-    }
-
-    computerRoom->add(tables);
-    computerRoom->add(new Item("Доска", 200));
-
-    office->add(reception);
-    office->add(room1);
-    office->add(computerRoom);
-
-    office->print();
-
-    cout << "\nОбщая стоимость: " << office->getPrice() << endl;
+    order1.setDelivery(new PostDelivery());
+    cout << order1.getDeliveryCost() << endl;
 
     return 0;
 }
